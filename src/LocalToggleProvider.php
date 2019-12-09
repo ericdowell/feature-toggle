d90be4d7f71e98f43f233cd0d1ddbc2ee749ff96
Eric Dowell <github@ericdowell.com>
2019-12-08 21:34:01 -0600

    Updating Session/Local providers to share code.

--- src/LocalToggleProvider.php
+++ src/LocalToggleProvider.php
@@ -4,36 +4,21 @@ declare(strict_types=1);
 
 namespace FeatureToggle;
 
-use FeatureToggle\Concerns\ToggleProvider;
 use FeatureToggle\Contracts\Toggle as ToggleContract;
-use FeatureToggle\Contracts\ToggleProvider as ToggleProviderContract;
 use FeatureToggle\Toggle\Local as LocalToggle;
 use Illuminate\Contracts\Config\Repository as ConfigContract;
-use Illuminate\Support\Collection;
 
-class LocalToggleProvider implements ToggleProviderContract
+class LocalToggleProvider extends AbstractToggleProvider
 {
-    use ToggleProvider;
-
-    /**
-     * @var \Illuminate\Contracts\Config\Repository
-     */
-    protected $config;
-
-    /**
-     * @var string
-     */
-    protected $key;
-
     /**
      * LocalToggleProvider constructor.
      *
-     * @param  \Illuminate\Contracts\Config\Repository  $config
+     * @param  \Illuminate\Contracts\Config\Repository  $repository
      * @param  string  $key
      */
-    public function __construct(ConfigContract $config, string $key = 'feature-toggle.toggles')
+    public function __construct(ConfigContract $repository, string $key = 'feature-toggle.toggles')
     {
-        $this->config = $config;
+        $this->repository = $repository;
         $this->key = $key;
     }
 
@@ -46,34 +31,12 @@ class LocalToggleProvider implements ToggleProviderContract
     }
 
     /**
-     * Get all toggles from config and normalize.
-     *
-     * @return ToggleContract[]|Collection
+     * @param  string  $name
+     * @param  mixed  $isActive
+     * @return LocalToggle
      */
-    protected function calculateToggles(): Collection
+    public function newRepositoryToggle(string $name, $isActive): ToggleContract
     {
-        $toggles = collect();
-
-        foreach ($this->calculateLocalToggles() as $name => $isActive) {
-            $toggles->put($name, new LocalToggle($name, $isActive));
-        }
-
-        return $toggles;
-    }
-
-    /**
-     * Pull feature toggles from the application config file.
-     *
-     * @return array
-     */
-    protected function calculateLocalToggles(): array
-    {
-        $localFeatures = $this->config->get($this->key, []);
-
-        if (! is_array($localFeatures)) {
-            return [];
-        }
-
-        return $localFeatures;
+        return new LocalToggle($name, $isActive);
     }
 }
